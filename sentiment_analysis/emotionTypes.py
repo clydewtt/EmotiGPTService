965deca5e825229d2f579ef3574802e122074311
DrewUnlimited <157051195+DrewUnlimited@users.noreply.github.com>
2024-09-14 11:35:51 -0400

    feat: Sentiment Analysis w/ Gpt 4o-mini

---
+++ sentiment_analysis/emotionTypes.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Emotions(Enum):
+    NEUTRAL = "Neutral"
+    HAPPY = "Happy"
+    SAD = "Sad"
+    ANGRY = "Angry"
+    DISGUSTED = "Disgusted"
+    SURPRISED = "Surprised"
+    FEARFUL = "Fearful"
