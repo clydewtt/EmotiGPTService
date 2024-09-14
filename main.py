9be39147719246202c6ad4c98fde2ec7bdfc809b
Clyde T <84462113+clydewtt@users.noreply.github.com>
2024-09-14 09:22:59 -0400

    Ticket(4): Create base API routing and middleware configuration (#1)
    * inital setup commit
    
    * add running instructions to readme
    
    * add pylint and use logger
    
    * minor tweak
    
    * add black config
    
    * minor tweak
    
    * t
    
    * change toml
    
    * change toml
    
    * change toml
    
    * Trigger CI
    
    * minor tweak
    
    ---------
    
    Co-authored-by: ClydeWTT <your_email>

---
+++ main.py
@@ -0,0 +1,50 @@
+"""
+Entry point for service
+"""
+
+import time
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware
+import uvicorn
+from logger import logger
+
+
+app = FastAPI()
+
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        start_time = time.time()
+        response = await call_next(request)
+        process_time = time.time() - start_time
+        formatted_process_time = f"{process_time:.4f}"
+        logger.info(
+            "Request: %s %s completed in %ss",
+            request.method,
+            request.url.path,
+            formatted_process_time,
+        )
+        return response
+
+
+app.add_middleware(LoggingMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Tentative
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],  # Tentative
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Connected to EmotiGPT-Service"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+    )
