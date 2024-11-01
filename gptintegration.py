99838662a6a9de4eaca7d31ee2bc37a99fcaa110
DrewUnlimited <drewkelly0320@gmail.com>
2024-11-01 14:29:54 -0400

    now passing tests

--- gptintegration.py
+++ gptintegration.py
@@ -1,32 +1,31 @@
+"""This is the main chatbot that takes sentiment and visual emotions in account"""
+
 import os
 import openai
 from dotenv import load_dotenv
-from sentiment_analysis import sentiment_analysis
-from emotion_types import Emotions
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 def gpt_integration(user_request, emotion_response, sentiment_response, conversation_history):
     conversation_history.append({"role": "user", "content": user_request})
 
     system_message = {
         "role": "system",
-        "content": f"""You are a normal chatbot that takes sentiment analysis as input which is {sentiment_response}.
-        You will also respond with the user's actual physical emotion in mind, which is {emotion_response}."""
+        "content": f"""You are a normal chatbot that takes 
+        sentiment analysis as input which is {sentiment_response}.
+        You will also respond with the user's actual 
+        physical emotion in mind, which is {emotion_response}.""",
     }
 
-    
-    if not any(msg['role'] == 'system' for msg in conversation_history):
+    if not any(msg["role"] == "system" for msg in conversation_history):
         conversation_history.insert(0, system_message)
 
     chat = openai.chat.completions.create(model="gpt-4o-mini", messages=conversation_history)
-    
+
     reply = chat.choices[0].message.content
 
     conversation_history.append({"role": "assistant", "content": reply})
 
     return reply, conversation_history
-
-
-
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 import uvicorn
-from logger import logger
 from pydantic import BaseModel
+from logger import logger
 from emotion_types import Emotions
 from sentiment_analysis import sentiment_analysis
 from gptintegration import gpt_integration
@@ -29,7 +29,8 @@ class LoggingMiddleware(BaseHTTPMiddleware):
             formatted_process_time,
         )
         return response
-    
+
+
 class EmotionRequest(BaseModel):
     user_input: str
     emotion_response: str
@@ -50,24 +51,31 @@ app.add_middleware(
 def read_root():
     return {"message": "Connected to EmotiGPT-Service"}
 
-conversation_history = []
 
 @app.post("/chat")
-def read_chat(request:EmotionRequest):
+def read_chat(request: EmotionRequest):
     user_request = request.user_input
     emotion_response = request.emotion_response
     if emotion_response not in Emotions:
-        raise HTTPException(status_code=404, detail=f"{emotion_response} is not an applicable emotion.")
-
+        raise HTTPException(
+            status_code=404, detail=f"{emotion_response} is not an applicable emotion."
+        )
+    conversation_history = []
     sentiment_response = sentiment_analysis(user_request)
 
-    reply, conversation_history = gpt_integration(user_request, emotion_response, sentiment_response, conversation_history)
-    
-    print(f"User Request: {user_request}, User's physical emotion: {emotion_response}, Text sentiment analysis: {sentiment_response}")
+    reply, conversations = gpt_integration(
+        user_request, emotion_response, sentiment_response, conversation_history
+    )
+
+    print(
+        f"""User Request: {user_request},
+        User's physical emotion: {emotion_response},
+        Text sentiment analysis: {sentiment_response}"""
+    )
     print(f"Assistant: {reply}")
 
-    return {"message":reply}
-        
+    return {"message": reply}, conversations
+
 
 if __name__ == "__main__":
     uvicorn.run(
